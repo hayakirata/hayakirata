56e0513c8e762effe6673be74282d758cadbdbe3
hayakirata <49411046+hayakirata@users.noreply.github.com>
2019-07-26 07:56:53 -0700

    Add files via upload

---
+++ js/Albums.js
@@ -0,0 +1,45 @@
+// this file contains the data we need for the gallery
+// The main object, "gallery" contains an array of album
+// each album contains an array of photos 
+// (plus a name and a thumbnail image)
+// The photos contain an image src and some metadata
+
+
+var gallery = {
+    albums :[
+    {name :"Interactivity with JavaScript",
+   university : "Coursera - University of Michigan",
+src:"Pdfs/Coursera 3MRL6DXR3WH3.pdf"
+},
+
+
+{
+    name :"Responsive Web Design",
+   university : "Coursera - University of London ",
+    src:"Pdfs/Coursera%20XSUXXP643HPH.pdf"
+},
+{
+    
+      name :"Introduction to HTML5",
+   university : "Coursera - University of Michigan", src:"Pdfs/Coursera%20QHTYWT674NZH.pdf" 
+},
+{
+        name :"Advanced Styling with Responsive Design",
+   university : "Coursera - University of Michigan",
+       src:"Pdfs/Coursera%209ZUKQTMHZ7UG.pdf"  
+}
+,
+    {   
+        name :"Introduction to structured Qurey Language ",
+   university : "Coursera - University of Michigan",
+      src:"Pdfs/Coursera%20S5AEN5QRWBJU.pdf"
+    }
+,
+
+{
+         name :"Building Web Applicationsin PHP",
+   university : "Coursera - University of Michigan",
+      src:"Pdfs/Coursera%20ZJEWN7N7893G.pdf"
+    
+}]};
+
