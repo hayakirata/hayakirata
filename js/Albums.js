151c4e270bc057944181b09113c434158560bbc3
hayakirata <49411046+hayakirata@users.noreply.github.com>
2019-07-26 02:53:49 -0700

    Update Albums.js

--- js/Albums.js
+++ js/Albums.js
@@ -1,45 +1,41 @@
-// this file contains the data we need for the gallery
-// The main object, "gallery" contains an array of album
-// each album contains an array of photos 
-// (plus a name and a thumbnail image)
-// The photos contain an image src and some metadata
+
 
 
 var gallery = {
     albums :[
     {name :"Interactivity with JavaScript",
    university : "Coursera - University of Michigan",
-src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
+href:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
 },
 
 
 {
     name :"Responsive Web Design",
    university : "Coursera - University of London ",
-    src:"docs/Coursera%20XSUXXP643HPH.pdf"
+    href:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
 },
 {
     
       name :"Introduction to HTML5",
-   university : "Coursera - University of Michigan", src:"docs/Coursera%20QHTYWT674NZH.pdf" 
+   university : "Coursera - University of Michigan", src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf" 
 },
 {
         name :"Advanced Styling with Responsive Design",
    university : "Coursera - University of Michigan",
-       src:"docs/Coursera%209ZUKQTMHZ7UG.pdf"  
+       src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"  
 }
 ,
     {   
         name :"Introduction to structured Qurey Language ",
    university : "Coursera - University of Michigan",
-      src:"docs/Coursera%20S5AEN5QRWBJU.pdf"
+      src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
     }
 ,
 
 {
          name :"Building Web Applicationsin PHP",
    university : "Coursera - University of Michigan",
-      src:"docs/Coursera%20ZJEWN7N7893G.pdf"
+      src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
     
 }]};
 
