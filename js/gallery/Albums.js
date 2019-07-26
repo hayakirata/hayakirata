705044d54be0b69e5ce09a81278ee6383a6d4fca
hayakirata <49411046+hayakirata@users.noreply.github.com>
2019-07-26 02:42:06 -0700

    Add files via upload

---
+++ js/gallery/Albums.js
@@ -0,0 +1,89 @@
+// this file contains the data we need for the gallery
+// The main object, "gallery" contains an array of album
+// each album contains an array of photos 
+// (plus a name and a thumbnail image)
+// The photos contain an image src and some metadata
+
+var gallery = {
+	albums : [
+		{
+			name : "Photograph", 
+			thumbnail : "img/8.jpg",
+			photos : [
+				{	
+					src : "img/1.jpg"
+
+				}, 
+				{	
+					src : "img/2.jpg"
+				}, 
+				{	
+				src : "img/3.jpg"
+				}, {
+			
+                   src : "img/5.jpg"
+				}, 
+				{	
+                   src : "img/6.jpg"
+				}, 
+				{	
+                   src : "img/7.jpg"
+				}, 
+				{	
+                   src : "img/8.jpg"
+				}, 
+				{	
+                   src : "img/9.jpg"
+				}, 
+				{	
+                   src : "img/10.jpg"
+				}, 
+				{	
+                   src : "img/11.jpg"
+				}, 
+				{	
+                   src : "img/13.jpg"
+				}, 
+				{	
+                   src : "img/14.jpg"
+				}, 
+
+				{	
+                   src : "img/16.jpg"
+				}, 
+				{	
+                   src : "img/17.jpg"
+				}
+                , 
+				{	
+                   src : "img/18.jpg"
+				}, 
+				{	
+                   src : "img/19.jpg"
+				}
+                , 
+				{	
+                   src : "img/21.jpg"
+				}
+			]
+		},
+		{
+			name : "Graphic Design", 
+			thumbnail : "img/graphic2.jpg",
+			photos : [
+				{	
+					src : "img/graphic1.jpg"
+				}, 
+				{	
+				src : "img/graphic2.jpg"
+				}, 
+				{	
+					src : "img/graphic3.jpg"
+				}  
+			]
+		}
+			]
+		}
+
+	
+;
