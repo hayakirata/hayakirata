c14754cfed5074aed224db44dfa8c44ad5ab4cf7
hayakirata <49411046+hayakirata@users.noreply.github.com>
2019-07-26 02:54:15 -0700

    Update Albums.js

--- js/Albums.js
+++ js/Albums.js
@@ -17,25 +17,25 @@ href:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
 {
     
       name :"Introduction to HTML5",
-   university : "Coursera - University of Michigan", src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf" 
+   university : "Coursera - University of Michigan", href:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf" 
 },
 {
         name :"Advanced Styling with Responsive Design",
    university : "Coursera - University of Michigan",
-       src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"  
+       href:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"  
 }
 ,
     {   
         name :"Introduction to structured Qurey Language ",
    university : "Coursera - University of Michigan",
-      src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
+      href:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
     }
 ,
 
 {
          name :"Building Web Applicationsin PHP",
    university : "Coursera - University of Michigan",
-      src:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
+      href:"https://github.com/hayakirata/hayakirata/Pdfs/Coursera%20XSUXXP643HPH.pdf"
     
 }]};
 
